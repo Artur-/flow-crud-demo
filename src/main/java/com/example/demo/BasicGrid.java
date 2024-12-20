7d59e771c71124731746200cd696286d24d73ef3
Artur Signell <artur@vaadin.com>
2024-12-20 10:54:09 +0200

    Move

--- src/main/java/com/example/demo/BasicGrid.java
+++ src/main/java/com/example/demo/BasicGrid.java
@@ -1,9 +1,4 @@
-package com.example.demo.views;
-
-import com.example.demo.Product;
-import com.example.demo.ProductRepository;
-import com.example.demo.ProductService;
-import org.springframework.data.domain.Pageable;
+package com.example.demo;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -12,9 +7,9 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
 
 @Route("")
-public class HomeView extends VerticalLayout {
+public class BasicGrid extends VerticalLayout {
 
-    public HomeView(ProductService productService, ProductRepository productRepository) {
+    public BasicGrid(ProductService productService, ProductRepository productRepository) {
         Grid<Product> grid = new Grid<>(Product.class, false);
         DataProvider<Product, Void> dataProvider = DataProvider.fromCallbacks(
                 query -> productService.list(VaadinSpringDataHelpers.toSpringPageRequest(query), null).stream(),
