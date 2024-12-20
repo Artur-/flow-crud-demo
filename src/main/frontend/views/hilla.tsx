9e05db7f8bfffa77818b03a5d9589dafdf110be6
Artur Signell <artur@vaadin.com>
2024-12-20 13:02:21 +0200

    Hilla view using the same service

--- src/main/frontend/views/hilla.tsx
+++ src/main/frontend/views/hilla.tsx
@@ -1,13 +1,17 @@
-import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+import { ViewConfig } from "@vaadin/hilla-file-router/types.js";
+import { ProductService } from "Frontend/generated/endpoints";
+import { Grid, GridSortColumn } from "@vaadin/react-components";
+import { useDataProvider } from "@vaadin/hilla-react-crud";
 
-export const config: ViewConfig = { menu: { title: 'Hilla' }};
+export const config: ViewConfig = { menu: { title: "Hilla" } };
 
 export default function Hilla() {
-    return <Grid dataProvider={useDataProvider(ProductService).dataProvider}>
-        <GridSortColumn path="name" />
-        <GridSortColumn path="price" />
-        <GridSortColumn path="category" />
-        
-
-    </Grid>)
+  return (
+    <Grid dataProvider={useDataProvider(ProductService).dataProvider}>
+      <GridSortColumn path="name" header="Name" />
+      <GridSortColumn path="description" header="Description" />
+      <GridSortColumn path="price" header="Price" />
+      <GridSortColumn path="stockQuantity" header="Stock Quantity" />
+    </Grid>
+  );
 }
--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.spring.data.jpa.CrudRepositoryService;
 import com.vaadin.hilla.BrowserCallable;
 
 @Service
 @BrowserCallable
+@AnonymousAllowed
 public class ProductService extends CrudRepositoryService<Product, Long, ProductRepository> {
     public ProductService(ProductRepository repository) {
         super(repository);
